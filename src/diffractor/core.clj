f411be1b4aa4575035d09ce03a5c8d25495f983d
Raymond W. Ko <raymond.w.ko@gmail.com>
2015-01-29 20:20:58 -0500

    initial commit of project template

---
+++ src/diffractor/core.clj
@@ -0,0 +1,7 @@
+(ns diffractor.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/diffractor/core_test.clj
@@ -0,0 +1,7 @@
+(ns diffractor.core-test
+  (:require [clojure.test :refer :all]
+            [diffractor.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
